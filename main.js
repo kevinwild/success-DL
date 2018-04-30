5ad45c2e2466f45cc5bab49dba22d5b065007ff5
Admin <kaw28@njit.edu>
2018-04-29 23:15:13 -0400

    Push to heroku

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 function start(){
     $("#startHere").fadeOut();
+    $("#part2").delay(750).fadeIn('slow');
 }
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+
+
+
+
+
+
+
