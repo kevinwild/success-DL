44893dba1c46be3e3b82baf0acca06f8de7aa019
Admin <kaw28@njit.edu>
2018-05-02 20:26:29 -0400

    Project is completed

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 function start(){
     $("#startHere").fadeOut();
     $("#part2").delay(750).fadeIn('slow');
+    successRepeat(50);
+}
+
+function successRepeat(count){
+    var phraseList =
+        [
+            "What is ",
+            "Who is ",
+            "How is ",
+            "How to ",
+            "Look for ",
+            "Look at",
+            "Feel ", "Make",
+            "Aspire for",
+            "Keeep "
+        ];
+    for (i = 0; i <= count; i++) {
+        var x = Math.floor(Math.random() * 10);
+        var ranString = phraseList[x] + " Success. <br>";
+        $("#successRepeat").append(ranString);
+
+    }
+
+
 }
 
 
@@ -11,7 +35,14 @@ function start(){
 
 
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
+    var links = [
+        ""
+
+
+    ];
+
+
+
 });
 
 
